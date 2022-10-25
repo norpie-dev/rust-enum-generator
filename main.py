5284952b8370fc8598fdde54d4c2f12be0267ecb
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-10-25 21:54:06 +0200

    final

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from os.path import exists
 TOP_ENUM = "use std::str::FromStr;\n\npub enum %name% {\n"
 BOT_ENUM = "}\n\n"
 
+TOP_FROM = "impl FromStr for %name% {\n\ttype Err = ();\n\tfn from_str(input: &str) -> Result<%name%, Self::Err> {\n\t\tmatch input {\n"
+BOT_FROM = "_ => Err(()),\n}\n}\n}"
+
 TOP_NAME = "impl %name% {\n\tpub fn name(&self) -> String { \n\t\tmatch *self {\n"
 BOT_NAME = "\t\t}\n\t}\n}"
 
@@ -12,7 +15,7 @@ def get_input():
         print("input.txt doesn't exist")
         return None
     with open('input.txt', 'r+') as file:
-        return [e.strip().split(" ") for e in file.readlines()]
+        return [e.strip().split(":") for e in file.readlines()]
 
 
 def main():
@@ -28,6 +31,11 @@ def main():
     for e in input_lines:
         lines.append("\t" + e[0] + ",\n")
     lines.append(BOT_ENUM)
+    lines.append("")
+    lines.append(TOP_FROM.replace("%name%", name))
+    for e in input_lines:
+        lines.append("\t\t\t\"" + e[0] + "\"" + " => " + name + "::" + e[0] + "\n")
+    lines.append(BOT_FROM)
     lines.append("")
     lines.append(TOP_NAME.replace("%name%", name))
     for e in input_lines:
